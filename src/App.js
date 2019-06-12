2e4d3b62bd4b420a10f68d543da310dec381c151
HassanBharu <hassan_7_8_6@hotmail.com>
2019-06-12 14:20:56 +0100

    add header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   state = {
-    array:[]
+    array: []
   }
+
   render() {
- return (
-    <div>
-    </div>
-  );
+    return (
+      <div>
+
+
+      </div>
+    );
 
   }
- 
+
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+
+const Header = () => {
+    return <h1 style={{ textAlign: 'center' }}>Find All Broken and Working Links</h1>
+}
+
+export default Header
